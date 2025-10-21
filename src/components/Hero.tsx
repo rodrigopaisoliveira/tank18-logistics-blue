5d1ec72fc85f3647d7d35251baaf2f43f728146b
rodrigopaisoliveira <rpoas@iscte-iul.pt>
2025-10-21 16:13:56 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
-import heroImage from "@/assets/hero-Tank.jpg";
+import heroImage from "@/assets/hero-tank.jpg";
 
 const Hero = () => {
   const scrollToContact = () => {
